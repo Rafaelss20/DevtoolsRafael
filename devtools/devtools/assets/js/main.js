99cadd19c837bb8c6ea05f9b3a30a77f2d228a7a
Rafaelss20 <135926563+Rafaelss20@users.noreply.github.com>
2024-05-06 19:47:00 -0300

    Add files via upload

---
+++ devtools/devtools/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+   // TEXTO DIGITADO
+   var typed = new Typed(".linguagens", {
+     strings: ["MIQUÉIAS SOUSA"],
+     typeSpeed: 100,
+     backSpeed: 60,
+     loop: true
+     });
+ });
+ 
